21bdb736b074d3938b9492f0ce492b446dbe58e1
gemario <gmario.sh@gmail.com>
2022-09-28 00:58:24 -0300

    Iniciando com React - Propriedades

---
+++ react-js-updated/content/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,21 @@
+import { Post } from './Post';
+
+export function App() {
+
+  return (
+    <div>
+      <Post 
+        author="Jorge Mario" 
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores accusamus placeat necessitatibus ex alias maxime ullam eius. Animi, nam perferendis esse possimus eaque praesentium eos accusamus, voluptatum repudiandae provident placeat!"
+      />
+      <Post 
+        author="Paulo Plinio"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta temporibus excepturi sit veniam, dolore minus? Quibusdam numquam quasi commodi iure magnam, dolorum soluta sit quam ad voluptatum illum perspiciatis asperiores?"
+      />
+      
+    </div>
+
+  )  
+}
+
+
