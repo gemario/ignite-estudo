a2cd9d5e60cfa1337b9da21780d2e3aeef6e4b8e
gemario <gmario.sh@gmail.com>
2022-09-29 02:59:05 -0300

    Estrutura da aplicação - Componente: Sidebar

--- react-js-updated/content/01-fundamentos-reactjs/src/App.jsx
+++ react-js-updated/content/01-fundamentos-reactjs/src/App.jsx
@@ -1,18 +1,33 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <div>
-      <Post 
-        author="Jorge Mario" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores accusamus placeat necessitatibus ex alias maxime ullam eius. Animi, nam perferendis esse possimus eaque praesentium eos accusamus, voluptatum repudiandae provident placeat!"
-      />
-      <Post 
-        author="Paulo Plinio"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta temporibus excepturi sit veniam, dolore minus? Quibusdam numquam quasi commodi iure magnam, dolorum soluta sit quam ad voluptatum illum perspiciatis asperiores?"
-      />
-      
+
+      <Header />
+
+      <div className={styles.wrapper}>
+       
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Jorge Mario" 
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores accusamus placeat necessitatibus ex alias maxime ullam eius. Animi, nam perferendis esse possimus eaque praesentium eos accusamus, voluptatum repudiandae provident placeat!"
+          />
+          <Post 
+            author="Paulo Plinio"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta temporibus excepturi sit veniam, dolore minus? Quibusdam numquam quasi commodi iure magnam, dolorum soluta sit quam ad voluptatum illum perspiciatis asperiores?"
+          />
+        </main>
+      </div>
     </div>
 
   )  
