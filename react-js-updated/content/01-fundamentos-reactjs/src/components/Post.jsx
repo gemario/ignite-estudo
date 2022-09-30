4f857da91f00a69ce056585881821b482dbb399e
gemario <gmario.sh@gmail.com>
2022-09-29 21:20:42 -0300

    Estrutura da aplicação - Componente: Post

---
+++ react-js-updated/content/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/gemario.png" />
+          <div className={styles.authorInfo}>
+            <strong>Jorge Mario</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='29 de Setembro às 20:53h' dateTime="2022-09-29 20:53:30">Públicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  );
+}
--- react-js-updated/content/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ react-js-updated/content/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar} 
+          src="https://github.com/gemario.png" 
+          alt="Foto de perfil do GitHub de Jorge Mario" 
+        />
+
         <strong>Jorge Mario</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
