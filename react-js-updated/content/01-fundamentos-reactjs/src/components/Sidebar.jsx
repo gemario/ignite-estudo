aa246c79805c1cc718c0c2bc177c7988a83d5bdc
gemario <gmario.sh@gmail.com>
2022-10-05 04:43:02 -0300

    Estrutura da aplicação - Aplicando responsividade

--- react-js-updated/content/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ react-js-updated/content/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,12 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar} 
-          src="https://github.com/gemario.png" 
-          alt="Foto de perfil do GitHub de Jorge Mario" 
-        />
-
+        <Avatar src="https://github.com/gemario.png" />
         <strong>Jorge Mario</strong>
         <span>Web Developer</span>
       </div>
