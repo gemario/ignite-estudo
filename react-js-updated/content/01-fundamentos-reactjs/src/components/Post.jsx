bdba389f14766c633149814017ec557a09bc6fbf
gemario <gmario.sh@gmail.com>
2022-10-09 04:00:22 -0300

    Os motores do React - Removendo comentários (imutabilidade)

--- react-js-updated/content/01-fundamentos-reactjs/src/components/Post.jsx
+++ react-js-updated/content/01-fundamentos-reactjs/src/components/Post.jsx
@@ -35,6 +35,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentWithoutDeletedOne = comments.filter(comment => {
+      return comment != commentToDelete;
+    })
+
+    setComments(commentWithoutDeletedOne);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -79,7 +87,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment 
+              key={comment} 
+              content={comment} 
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
